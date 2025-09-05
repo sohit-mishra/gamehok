daf52a46bfac4141db5b70f93f084853fba4191d
Sohit Mishra <7836020160s@gmail.com>
2025-09-05 15:35:52 +0530

    complete

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full text-center">
       <h1 className="text-6xl font-bold text-2xl mb-4">404 - Page Not Found</h1>
       <p className="text-lg mb-6">Sorry, the page you are looking for does not exist.</p>
-      <a
+      <Link
         href="/"
         className="text-white hover:underline text-lg"
       >
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
